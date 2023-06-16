d5ab64fbd69892b7611656db19641495d05157a1
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-06-16 15:21:59 +0200

    We want to Submit a form to database. Lets go

--- src/app/dbContent/page.tsx
+++ src/app/dbContent/page.tsx
@@ -4,9 +4,8 @@ import { PrismaClient } from '@prisma/client';
 // I want to paste DB content to this page.tsx
 // #########################################################
 
-
 // WIP - I dont know what im doing
-export async function getStaticProps() {
+export async function getData() {
     const prisma = new PrismaClient();
     const heroes = await prisma.heroes.findMany()
     console.log(heroes)
@@ -15,17 +14,46 @@ export async function getStaticProps() {
     };
 }
 
+export async function saveData({hero}) {
+    const prisma = new PrismaClient();
+    const result = await prisma.heroes.create({
+        data: {
+            name: {hero}
+        }
+    })
+
+}
+
 export default async function Page() {
-    const data = await getStaticProps()
+    saveData("test")
+    const data = await getData()
     const heroes = data.props.heroes
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         And we are in
+
+        <Form/>
+
         <ul>
             {heroes.map(hero => (
                 <li key={hero.id}>{hero.name}</li>
             ))}
         </ul>
+
+
+
     </main>
     );
+}
+
+
+function Form() {
+    return (
+        <form action="/api/form" method="post">
+            <label htmlFor="hero">Hero Name</label>
+            <input type="text" id="hero" name="hero" required />
+
+            <button type="submit">Save</button>
+        </form>
+    )
 }
