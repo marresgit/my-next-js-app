fbca99732c9546c7ca3340fda5d7bbc3bac760ac
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-06-17 23:56:05 +0200

    TypeError: useState.. using it wrong..

--- src/app/dbContent/page.tsx
+++ src/app/dbContent/page.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { PrismaClient } from '@prisma/client';
 import {Simulate} from "react-dom/test-utils";
 import error = Simulate.error;
-import {useState} from "react";
+import { useState } from 'react';
 
 
 export async function getData() {
@@ -14,8 +15,12 @@ export async function getData() {
 }
 
 
-export default async function Page() {
+interface FormData {
+    name: string
+    id: string
+}
 
+export default async function Page() {
     async function create(data: FormData) {
         try {
             fetch('http://localhost:3000/api/create', {
@@ -40,32 +45,11 @@ export default async function Page() {
 
     const data = await getData()
     const heroes = data.props.heroes
+    const [form, setForm] = useState<FormData>({name: '', id: ''});
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         And we are in
 
-        <Form/>
-
-        <ul>
-            {heroes.map(hero => (
-                <li key={hero.id}>{hero.name}</li>
-            ))}
-        </ul>
-
-
-
-    </main>
-    );
-}
-
-interface FormData {
-    name: string
-    id: string
-}
-
-function Form() {
-    const [form, setForm] = useState<FormData>({name: '', id: ''})
-    return (
         <div>
             <h1 className="text-center font-bold text-2x1 mt-4">Data</h1>
             <form onSubmit={e => {
@@ -81,5 +65,36 @@ function Form() {
 
             </form>
         </div>
-    )
+
+        <ul>
+            {heroes.map(hero => (
+                <li key={hero.id}>{hero.name}</li>
+            ))}
+        </ul>
+
+
+
+    </main>
+    );
 }
+
+// function Form() {
+//     const [form, setForm] = useState<FormData>({name: '', id: ''})
+//     return (
+//         <div>
+//             <h1 className="text-center font-bold text-2x1 mt-4">Data</h1>
+//             <form onSubmit={e => {
+//                 e.preventDefault()
+//             }} className='w-auto min-w-[25%] max-w-min mx-auto space-y-6 flex flex-col items-stretch'>
+//                 <input type="text"
+//                        placeholder="Name"
+//                        value={form.name}
+//                        onChange={e => setForm({...form, name: e.target.value})}
+//                        className="border-2 rounded border-gray-600 p-1"
+//                 />
+//                 <button type="submit" className="bg-blue-500 text-white rounded p-1">Add</button>
+//
+//             </form>
+//         </div>
+//     )
+// }
