3a1976d8c4e081e7eb552de506cfae8b3c5f1eb6
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-06-20 11:59:22 +0200

    small fixes

--- src/app/dotaApp/page.tsx
+++ src/app/dotaApp/page.tsx
@@ -11,16 +11,15 @@ export async function getData() {
 export default async function Page() {
     const data = await getData()
     const heroes = data.props.heroes
-    // const [form, setForm] = useState<FormData>({name: '', id: ''});
     return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        And we are in
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>The Dota app</h1>
 
-        <ul>
-            {heroes.map(hero => (
-                <li key={hero.id}>{hero.name}</li>
-            ))}
-        </ul>
-    </main>
+            <ul>
+                {heroes.map(hero => (
+                    <li key={hero.id}>{hero.name}</li>
+                ))}
+            </ul>
+        </main>
     );
 }
