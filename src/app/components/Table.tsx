b4956aa42284c13497aa32f2d71ea662c029f724
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-06-26 23:02:30 +0200

    Better looking table

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -3,32 +3,34 @@
 
 export default function Table() {
     return (
-        <table className="table-auto">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Created</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
+        <div className="bg-white pt-8 pb-6 shadow rounded-lg sm-px-10 pl-4 pr-4">
+            <table className="table-auto text-black">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Created</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
 
-                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                <td>Malcolm Lockyer</td>
-                <td>1961</td>
-            </tr>
-            <tr>
-                <td>Witchy Woman</td>
-                <td>The Eagles</td>
-                <td>1972</td>
-            </tr>
-            <tr>
-                <td>Shining Star</td>
-                <td>Earth, Wind, and Fire</td>
-                <td>1975</td>
-            </tr>
-            </tbody>
-        </table>
+                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    <td>Malcolm Lockyer</td>
+                    <td>1961</td>
+                </tr>
+                <tr>
+                    <td>Witchy Woman</td>
+                    <td>The Eagles</td>
+                    <td>1972</td>
+                </tr>
+                <tr>
+                    <td>Shining Star</td>
+                    <td>Earth, Wind, and Fire</td>
+                    <td>1975</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
