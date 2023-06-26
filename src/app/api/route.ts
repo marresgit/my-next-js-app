00c824d8ccc218490f677ee2dda83eafe3202c96
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-06-26 13:29:53 +0200

    Creating API structure with routes (GET/POST(WIP))

---
+++ src/app/api/route.ts
@@ -0,0 +1,36 @@
+import {NextRequest, NextResponse} from 'next/server'
+import {prisma} from '@/db'
+import {json} from "stream/consumers";
+import {Prisma} from ".prisma/client";
+import PrismaClientKnownRequestError = Prisma.PrismaClientKnownRequestError;
+
+export async function GET() {
+    const heroes = await prisma.heroes.findMany()
+
+    return NextResponse.json({heroes})
+}
+
+export async function POST(request: NextRequest) {
+    const data = await request.json()
+    const hero = {
+        name: data.name
+    }
+
+    console.log(data)
+    try {
+        const result = await prisma.heroes.create({
+            data: {
+                name: hero.name
+            }
+        })
+        return NextResponse.json({result})
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === 'P2002') {
+                console.log("There is an unique constraint validation!!!")
+            }
+        }
+        throw e
+    }
+}
+
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,16 +1,17 @@
-import { prisma } from './../../db'
-
-export async function getData() {
-    const heroes = await prisma.heroes.findMany()
-    console.log(heroes)
-    return {
-        props : { heroes },
-    };
-}
+'use client';
 
 export default async function Page() {
-    const data = await getData()
-    const heroes = data.props.heroes
+    const res = await fetch('http://localhost:3000/api', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+
+    const data = await res.json()
+
+    const heroes = data.heroes;
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>The Dota app</h1>
@@ -20,6 +21,8 @@ export default async function Page() {
                     <li key={hero.id}>{hero.name}</li>
                 ))}
             </ul>
+
+
         </main>
-    );
+    )
 }
